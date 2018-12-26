d3fa019b305ec9e0e62c8b71de2e7c3ca3cb12bf
JamaisMagic <1350756140@qq.com>
2018-12-26 23:30:46 +0800

    Update service worker.

--- app/f2e/service-worker.js
+++ app/f2e/service-worker.js
@@ -4,11 +4,28 @@ console.log(self.__precacheManifest);
 
 
 self.addEventListener('push', function (event) {
-  console.log(event);
+  console.log(event.data);
+  const payload = event.data ? event.data.text() : '';
+  if (!payload) {
+    return;
+  }
   event.waitUntil(
     self.registration.showNotification('ServiceWorker received push', {
-      body: 'Yes, you do.',
+      actions: {
+        action: 'action',
+        title: 'actions title',
+        // icon: ''
+      },
+      // badge: '',
+      body: payload,
+      // icon: '',
+      // image: '',
+      renotify: false,
+      requireInteraction: false,
     })
+      .then(event => {
+        console.log(event.type);
+      })
   );
 });
 
