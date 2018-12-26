c9517c32e158446f9a05e17dcfae0982c741f0c7
JamaisMagic <1350756140@qq.com>
2018-12-26 23:13:25 +0800

    Add log for service worker.

--- app/f2e/service-worker.js
+++ app/f2e/service-worker.js
@@ -4,6 +4,7 @@ console.log(self.__precacheManifest);
 
 
 self.addEventListener('push', function (event) {
+  console.log(event);
   event.waitUntil(
     self.registration.showNotification('ServiceWorker received push', {
       body: 'Yes, you do.',
