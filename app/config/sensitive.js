36fd36eaedfb24ae83183855e2ddd5f8bda4545a
JamaisMagic <1350756140@qq.com>
2019-01-02 13:02:07 +0800

    Update vapid.

--- app/config/sensitive.js
+++ app/config/sensitive.js
@@ -1,6 +1,6 @@
 module.exports = {
   vapid: {
-    "publicKey": "",
-    "privateKey": ""
+    "publicKey": "BC8OkdboFO_cuonafMmGUDZLwIQaUqG3XJrGoaajniesg_wJTidw0ttPGenM1S-S26IgHvJ5Vpr--AWWsZ_K-6M",
+    "privateKey": "VoW_aeb33HLVSUAR6VDqo_U6WxlhT5l8keo6EnsWcl0"
   }
 };
